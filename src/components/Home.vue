73d7db23b0213137fd1d41fb92266d82a45eb10f
Gechrist <gchris@hotmail.co.uk>
2022-09-04 11:24:30 +0300

    sidebar width

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,11 +6,11 @@ import Texts from './Texts.vue';
 
 <template>
   <div class="flex flex-row">
-    <section class="w-4/6 flex flex-col">
+    <section class="w-[65%] md:w-4/6 flex flex-col">
       <Nav />
       <Texts />
     </section>
-    <section class="bg-[#212834] w-2/6 h-auto">
+    <section class="bg-[#212834] w-[35%] md:w-2/6 h-auto">
       <Img />
     </section>
   </div>
