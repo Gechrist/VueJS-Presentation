e49d7df8cee7334b8d3cfaa0360f147cf99afba8
Gechrist <gchris@hotmail.co.uk>
2022-09-07 14:50:04 +0300

    test version 2

--- src/components/Image.vue
+++ src/components/Image.vue
@@ -4,20 +4,20 @@ const contactInfo = 'Lorem Ipsum etc etc';
 </script>
 
 <template>
-<<<<<<< Updated upstream
   <div class="flex flex-col text-white">
-=======
-  <aside class="flex flex-col text-white">
->>>>>>> Stashed changes
-    <img
-      class="w-32 h-32 mx-auto mt-4 rounded-full md:w-72 md:h-72"
-      src="../assets/featuredImage.jpg"
-      alt="Nikolaos Mavropoulos Portrait"
-    />
-    <h4 class="text-center mt-2 text-xl">{{ name }}</h4>
-    <div class="ml-2">
-      <h4 class="text-left mt-8 underline underline-offset-2">Contact Info:</h4>
-      <p>{{ contactInfo }}</p>
-    </div>
+    <aside class="flex flex-col text-white">
+      <img
+        class="w-32 h-32 mx-auto mt-4 rounded-full md:w-72 md:h-72"
+        src="../assets/featuredImage.jpg"
+        alt="Nikolaos Mavropoulos Portrait"
+      />
+      <h4 class="text-center mt-2 text-xl">{{ name }}</h4>
+      <div class="ml-2">
+        <h4 class="text-left mt-8 underline underline-offset-2">
+          Contact Info:
+        </h4>
+        <p>{{ contactInfo }}</p>
+      </div>
+    </aside>
   </div>
 </template>
